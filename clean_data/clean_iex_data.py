d626ba2f6977a87876e223eb1332c6147ddabf4d
justinminsk <justin.minsk@gmail.com>
2019-01-24 16:47:16 -0500

    need to use a compute engine for the twitter data

--- clean_data/clean_iex_data.py
+++ clean_data/clean_iex_data.py
@@ -1,12 +1,18 @@
 import pandas as pd 
+from dateutil.parser import *
 
 df = pd.read_csv("~/repo/thesis/iex/mintue_trade_data.csv", na_values=[-1])
 
 df = df.fillna(method="ffill")
 
-print(df.head(50))
-
 df.to_pickle("./iex_clean.pkl")
 
 # TODO: Get time in python datetime and also only have averge saved on own to_pickle
 # TODO: Look into if day by day is useful for non-trading days
+
+date_df = pd.DataFrame({"date": df.date.astype(str) + " " + df.minute, "stock_price_col" : df.average})
+date_df.date = date_df.date.apply(parse)
+
+print(date_df.head())
+
+date_df.to_pickle("./date_iex_data.pkl")
--- clean_data/get_bq_data.py
+++ clean_data/get_bq_data.py
@@ -6,42 +6,55 @@ from google.cloud import bigquery
 from nltk.corpus import stopwords
 from nltk.tokenize import TweetTokenizer
 from nltk.stem.porter import PorterStemmer
+from nltk.util import ngrams
 
 
-# nltk.download('punkt')
-# nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('stopwords')
 
 
 # Get Tweets from bigquery
 client = bigquery.Client()
 
-query = "SELECT created_at, id_str, text, truncated, user.verified, user.followers_count, user.favourites_count, entities.urls FROM `jminsk-thesis.twitter.tweets2` WHERE lang='en' LIMIT 100"
+query = "SELECT created_at, id_str, text, truncated, user.verified, user.followers_count, user.favourites_count, entities.urls FROM `jminsk-thesis.twitter.tweets2` WHERE lang='en'"
 
 df = pd.io.gbq.read_gbq(query, project_id="jminsk-thesis", dialect="standard")
 
-# process non word dataframe
+# get rid of date/times that are not part of the minute by minute price data
+date_df = pd.read_pickle("./date_iex_data.pkl")
+
+df.created_at = df.created_at.map(lambda x: x.replace(second=0))
+df = df.rename(index=str, columns={"created_at": "date"})
+date_df.date = date_df.date - pd.Timedelta(minutes=1)
+
+df = pd.merge(df, date_df, on="date", how="outer")
+
+del date_df
+
+# process urls to make space
 df.urls = df.urls.str.len()
 df = df.fillna(0)
-
-# print(df.ix[6])
-
-# print(df.text[6])
 
 # start processing words
 nltk_stopwords = stopwords.words("english")
 tknzr = TweetTokenizer(preserve_case=False)
 stemmer = PorterStemmer()
 
+df.text = df.text.astype(str)
+
 def preprocess(x):
     x = re.sub(r"http\S+", "", x)
     x = tknzr.tokenize(x)
     x = [w for w in x if w not in set(nltk_stopwords)]
     x = [stemmer.stem(w) for w in x]
+    bigrams = ngrams(x, 2)
+    bigrams = ["_".join(list(gram)) for gram in bigrams]
+    trigrams = ngrams(x, 3)
+    trigrams = ["_".join(list(gram)) for gram in trigrams]
+    x = x + bigrams + trigrams
     return x
 
 df.text = df.text.apply(preprocess)
-
-# print(df)
 
 # one hot the data
 # https://stackoverflow.com/questions/45312377/how-to-one-hot-encode-from-a-pandas-column-containing-a-list
@@ -62,12 +75,13 @@ def pir_fast(df):
 # one hot the text
 df = pir_fast(df)
 
-# one hot the str ids
-df = pd.get_dummies(df, "str_id")
-
 # replace true and false with 1 and 0
 df.truncated = df.truncated.astype(int)
 df.verified = df.verified.astype(int)
 
-# TODO: combine price to tweet dataframe, maybe sort tweets by vaild trade times
-# TODO: add bi adn tri grams use nltk ngrams
+# one hot the str ids
+df = pd.get_dummies(df, "str_id")
+
+# write to a new gbq
+
+pd.io.gbq.to_gbq(df, "jminsk-thesis.twitter.clean_twitter")
