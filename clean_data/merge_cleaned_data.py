c4af41eeea06a431ae624276d708f0d69c77e1d9
justinminsk <justin.minsk@gmail.com>
2019-01-25 18:34:18 -0500

    if i get all of the files this new pythons script should put all the data in gbq so that i can grab peaices of the table

---
+++ clean_data/merge_cleaned_data.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+from google.cloud import bigquery
+from google.cloud import storage
+
+
+def list_blobs(bucket_name):
+    """Lists all the blobs in the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+
+    blobs = bucket.list_blobs()
+    return blobs
+
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    print('File {} uploaded to {}.'.format(
+        source_file_name,
+        destination_blob_name))
+
+blob_list = list_blobs("jminsk-thesis/tweeterdata")
+
+final_df = pd.DataFrame()
+
+for blob in blob_list:
+    upload_blob("jminsk-thesis/tweeterdata", blob, "temp.pkl")
+    df = pd.read_pickle("temp.pkl")
+
+    final_df = pd.concat([final_df, df]).fillna(0).to_sparse(fill_value=0)
+
+final_df.to_gbq(project_id="jminsk-thesis", destination_table="twitter.clean_twitter_data", if_exists='replace')
+    
