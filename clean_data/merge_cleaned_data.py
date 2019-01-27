1f89950759bab92ce7ab4fb2391e471e9adceb58
justinminsk <justin.minsk@gmail.com>
2019-01-26 20:43:19 -0500

    cahnge a small mistake in code

--- clean_data/merge_cleaned_data.py
+++ clean_data/merge_cleaned_data.py
@@ -6,6 +6,27 @@ from google.cloud import bigquery
 from google.cloud import storage
 
 
+def list_blobs_with_prefix(bucket_name, prefix, delimiter=None):
+    """Lists all the blobs in the bucket that begin with the prefix.
+    This can be used to list all blobs in a "folder", e.g. "public/".
+    The delimiter argument can be used to restrict the results to only the
+    "files" in the given "folder". Without the delimiter, the entire tree under
+    the prefix is returned. For example, given these blobs:
+        /a/1.txt
+        /a/b/2.txt
+    If you just specify prefix = '/a', you'll get back:
+        /a/1.txt
+        /a/b/2.txt
+    However, if you specify prefix='/a' and delimiter='/', you'll get back:
+        /a/1.txt
+    """
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+
+    blobs = bucket.list_blobs(prefix=prefix, delimiter=delimiter)
+
+    return blobs
+
 def list_blobs(bucket_name):
     """Lists all the blobs in the bucket."""
     storage_client = storage.Client()
@@ -26,7 +47,7 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
         source_file_name,
         destination_blob_name))
 
-blob_list = list_blobs("jminsk-thesis/tweeterdata")
+blob_list = list_blobs_with_prefix("jminsk-thesis", "/tweeterdata")
 
 final_df = pd.DataFrame()
 
