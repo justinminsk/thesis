ddc758e5d72ce5d7b40f92c346fa1c9a6379a17c
justinminsk <justin.minsk@gmail.com>
2019-02-04 12:51:41 -0500

    fix up bash scrpits

--- clean_data/data_prep.sh
+++ clean_data/data_prep.sh
@@ -9,6 +9,6 @@ gsutil cp iex_clean.parquet gs://jminsk_thesis/iex_data/iex_clean.parquet
 gsutil cp date_iex_data.parquet gs://jminsk_thesis/iex_data/date_iex_data.parquet
 gsutil cp user_list.parquet gs://jminsk_thesis/user_list/user_list.parquet
 
-python3 split_data_by_day.py
+# python3 split_data_by_day.py
 
-gsutil -m cp -r ./day_data gs://jminsk_thesis/
+# gsutil -m cp -r ./day_data gs://jminsk_thesis/
