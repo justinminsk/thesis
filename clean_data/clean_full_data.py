7bf69deffddec3a1b0098ae64ac6030670150cad
justinminsk <justin.minsk@gmail.com>
2019-02-07 15:53:59 -0500

    no clue why udf does not work at all

--- clean_data/clean_full_data.py
+++ clean_data/clean_full_data.py
@@ -34,7 +34,7 @@ def pre_processing(column):
     return result.strip()
 
 def build_pipeline():
-    tokenizer = [Tokenizer(inputCol='tweet',outputCol='words')]
+    tokenizer = [Tokenizer(inputCol='text',outputCol='words')]
     ngrams = [NGram(n=i, inputCol='words', outputCol='{0}_grams'.format(i)) for i in range(1,6)]
     cv = [CountVectorizer(vocabSize=100000, inputCol='{0}_grams'.format(i), outputCol='{0}_tf'.format(i)) for i in range(1,6)]
     idf = [IDF(inputCol='{0}_tf'.format(i), outputCol='{0}_tfidf'.format(i), minDocFreq=5) for i in range(1,6)]
