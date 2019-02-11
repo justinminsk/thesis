38ea84d8cf8f5408a9ebffadc502d8a6787fed2b
justinminsk <justin.minsk@gmail.com>
2019-02-10 21:25:39 -0500

    back to trying to make pandas df more print statements

--- clean_data/spark_make_pandas_df.py
+++ clean_data/spark_make_pandas_df.py
@@ -5,6 +5,7 @@ import pyspark as ps
 from pyspark.sql import functions as f
 from pyspark.sql.types import StringType, TimestampType
 
+# gcloud dataproc clusters create twitter-spark --image-version 1.3 --master-machine-type n1-standard-8 --worker-machine-type n1-standard-8 --metadata 'PIP_PACKAGES=pandas==0.23.0 scipy==1.1.0' --initialization-actions gs://dataproc-initialization-actions/python/pip-install.sh 
 
 inputdir = "gs://jminsk_thesis/"
 outputdir= "gs://jminsk_thesis/"
@@ -26,30 +27,40 @@ if __name__ == "__main__":
     dates = ("2018-01-01",  "2018-12-26")
     date_from, date_to = [f.to_date(f.lit(s)).cast(TimestampType()) for s in dates]
     temp_df = df.where((df.date_col > date_from) & (df.date_col < date_to))
+    print(temp_df.show(5))
     temp_df = temp_df.toPandas()
+    print(temp_df.head())
     temp_df.to_parquet(outputdir+"processed_twitter_train_data_1")
     print("Create Train DF 2")
     dates = ("2018-12-25",  "2019-01-01")
     date_from, date_to = [f.to_date(f.lit(s)).cast(TimestampType()) for s in dates]
     temp_df = df.where((df.date_col > date_from) & (df.date_col < date_to))
+    print(temp_df.show(5))
     temp_df = temp_df.toPandas()
+    print(temp_df.head())
     temp_df.to_parquet(outputdir+"processed_twitter_train_data_2")
     print("Create Test DF 1")
     dates = ("2018-12-31",  "2019-01-13")
     date_from, date_to = [f.to_date(f.lit(s)).cast(TimestampType()) for s in dates]
     temp_df = df.where((df.date_col > date_from) & (df.date_col < date_to))
+    print(temp_df.show(5))
     temp_df = temp_df.toPandas()
+    print(temp_df.head())
     temp_df.to_parquet(outputdir+"processed_twitter_test_data_1")
     print("Create Test DF 1")
     dates = ("2019-01-12",  "2019-01-23")
     date_from, date_to = [f.to_date(f.lit(s)).cast(TimestampType()) for s in dates]
     temp_df = df.where((df.date_col > date_from) & (df.date_col < date_to))
+    print(temp_df.show(5))
     temp_df = temp_df.toPandas()
+    print(temp_df.head())
     temp_df.to_parquet(outputdir+"processed_twitter_test_data_2")
     print("Create Val DF")
     dates = ("2019-01-22",  "2019-01-25")
     date_from, date_to = [f.to_date(f.lit(s)).cast(TimestampType()) for s in dates]
     temp_df = df.where((df.date_col > date_from) & (df.date_col < date_to))
+    print(temp_df.show(5))
     temp_df = temp_df.toPandas()
+    print(temp_df.head())
     temp_df.to_parquet(outputdir+"processed_twitter_val_data")
     sc.stop()
