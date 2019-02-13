d95488d302b25bd030a8eb4f88056e4ffcb1dffc
justinminsk <justin.minsk@gmail.com>
2019-02-13 16:16:45 -0500

    not sure why it is not working

--- models/iex_data_model.py
+++ models/iex_data_model.py
@@ -6,7 +6,7 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.python.keras.initializers import RandomUniform
 from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Input, Dense, Embedding, LSTM, GRU
+from tensorflow.python.keras.layers import Input, Dense, Dropout, Embedding, LSTM, GRU
 from tensorflow.python.keras.optimizers import RMSprop
 from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard, ReduceLROnPlateau
 
@@ -41,7 +41,7 @@ y_data = y_data.reshape(y_data.shape[0], 1)
 print("Shape y_data:", y_data.shape)
 
 num_x_signals = x_data.shape[1]
-num_y_signals = y_data.shape[1]
+num_y_signals = 1
 
 print("Spliting List")
 
@@ -61,8 +61,8 @@ x_train_scaled = x_scaler.fit_transform(x_train)
 x_test_scaled = x_scaler.transform(x_test)
 
 y_scaler = MinMaxScaler()
-y_train_scaled = y_scaler.fit_transform(y_train).reshape(y_train.shape[0],)
-y_test_scaled = y_scaler.transform(y_test).reshape(y_test.shape[0],)
+y_train_scaled = y_scaler.fit_transform(y_train)
+y_test_scaled = y_scaler.transform(y_test)
 
 print("x Train:",x_train_scaled.shape)
 print("y Trian", y_train_scaled.shape)
@@ -79,7 +79,7 @@ def batch_generator(batch_size, sequence_length):
         x_batch = np.zeros(shape=x_shape, dtype=np.float16)
 
         # Allocate a new array for the batch of output-signals.
-        y_shape = (batch_size, sequence_length)
+        y_shape = (batch_size, sequence_length, num_y_signals)
         y_batch = np.zeros(shape=y_shape, dtype=np.float16)
 
         # Fill the batch with random sequences of data.
@@ -101,25 +101,67 @@ sequence_length = 7200
 generator = batch_generator(batch_size=batch_size,
                             sequence_length=sequence_length)
 
-# x_batch, y_batch = next(generator)
+x_batch, y_batch = next(generator)
 
-# print(x_batch.shape)
-# print(y_batch.shape)
+print(x_batch.shape)
+print(y_batch.shape)
 
 validation_data = (np.expand_dims(x_test_scaled, axis=0),
                    np.expand_dims(y_test_scaled, axis=0))
 
 print("Build Model")
 
+warmup_steps = 20
+
+def loss_mse_warmup(y_true, y_pred):
+    """
+    Calculate the Mean Squared Error between y_true and y_pred,
+    but ignore the beginning "warmup" part of the sequences.
+    
+    y_true is the desired output.
+    y_pred is the model's output.
+    """
+
+    # The shape of both input tensors are:
+    # [batch_size, sequence_length, num_y_signals].
+
+    # Ignore the "warmup" parts of the sequences
+    # by taking slices of the tensors.
+    y_true_slice = y_true[:, warmup_steps:, :]
+    y_pred_slice = y_pred[:, warmup_steps:, :]
+
+    # These sliced tensors both have this shape:
+    # [batch_size, sequence_length - warmup_steps, num_y_signals]
+
+    # Calculate the MSE loss for each value in these tensors.
+    # This outputs a 3-rank tensor of the same shape.
+    loss = tf.losses.mean_squared_error(labels=y_true_slice,
+                                        predictions=y_pred_slice)
+
+    # Keras may reduce this across the first axis (the batch)
+    # but the semantics are unclear, so to be sure we use
+    # the loss across the entire tensor, we reduce it to a
+    # single scalar with the mean function.
+    loss_mean = tf.reduce_mean(loss)
+
+    return loss_mean
+
 # an lstm to a gru to a dense output
 model = Sequential()
-model.add(LSTM(units=200, return_sequences=True, input_shape=(None, num_x_signals,)))
-model.add(GRU(100, return_sequences=False))
+model.add(LSTM(units=200,
+              return_sequences=True,
+              input_shape=(None, num_x_signals,)))
+model.add(Dropout(0.2))
+model.add(GRU(100))
+model.add(Dropout(0.2))
+
 init = RandomUniform(minval=-0.05, maxval=0.05)
-model.add(Dense(num_y_signals,  kernel_initializer='normal'))
+model.add(Dense(num_y_signals,
+                    activation='linear',
+                    kernel_initializer=init))
 
 optimizer = RMSprop(lr=1e-3)
-model.compile(loss='mean_squared_error', optimizer=optimizer)
+model.compile(loss=loss_mse_warmup, optimizer=optimizer)
 
 print(model.summary())
 
