9a9ce4bcab09aa9b4892d57d5b860ec431c44039
justinminsk <justin.minsk@gmail.com>
2019-02-15 10:18:45 -0500

    change bash files have data ready for spark

--- clean_data/get_data.sh
+++ clean_data/get_data.sh
@@ -5,5 +5,3 @@ gsutil cp gs://jminsk_thesis/iex_data/iex_clean.parquet iex_clean.parquet
 gsutil cp gs://jminsk_thesis/iex_data/date_iex_data.parquet date_iex_data.parquet
 
 gsutil -m cp -r gs://jminsk_thesis/twitter_data.parquet twitter_data.parquet
-
-gsutil -m cp gs://jminsk_thesis/processed_twitter_data.parquet .
