52b415d676d699637b90edb2cf38e8e41b6c54ac
justinminsk <justin.minsk@gmail.com>
2019-02-26 11:18:06 -0500

    use less nodes... this might mean i need a really simple model

--- models/wallstreet_data_model.py
+++ models/wallstreet_data_model.py
@@ -129,7 +129,7 @@ def loss_mse_warmup(y_true, y_pred):
 
 # an lstm to a gru to a dense output
 model = Sequential()
-model.add(GRU(units=200, return_sequences=True, input_shape=(None, num_x_signals,)))
+model.add(GRU(units=100, return_sequences=True, input_shape=(None, num_x_signals,)))
 # model.add(Dropout(0.2))
 # model.add(LSTM(100, return_sequences=True))
 # model.add(Dropout(0.2))
