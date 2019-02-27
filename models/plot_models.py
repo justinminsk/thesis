a7d0ef5e29f33e5d096f8a3d6cf8cb6d7000fc51
justinminsk <justin.minsk@gmail.com>
2019-02-27 15:07:22 -0500

    change  an import

--- models/plot_models.py
+++ models/plot_models.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.externals import joblib
-from tf.keras.models import load_model
+from tensorflow.keras.models import load_model
 
 
 def get_iex_data():
